70350031e1857910f8c1bd2914cee535373748c4
Shivang Dudani <shivangdudani113@gmail.com>
2025-12-22 11:35:20 +0530

    changing default neft to NEFT

--- mock-service/src/config/mock-config/TRV11/BUS/2.0.0/on_init/generator.ts
+++ mock-service/src/config/mock-config/TRV11/BUS/2.0.0/on_init/generator.ts
@@ -59,7 +59,7 @@ export async function onInitGenerator(
               descriptor: {
                 code: "SETTLEMENT_TYPE",
               },
-              value: "neft",
+              value: "NEFT",
             },
             {
               descriptor: {
