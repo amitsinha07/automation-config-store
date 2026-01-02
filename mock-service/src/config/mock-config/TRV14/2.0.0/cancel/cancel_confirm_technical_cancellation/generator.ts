0ee966a4f4bbc3a561ac4cb7666c034d98e362b3
Sharique <sharique@WILASSETA0046.local>
2026-01-02 17:44:44 +0530

    fix: technical cancellayion issue in TRV14 in dev env

--- mock-service/src/config/mock-config/TRV14/2.0.0/cancel/cancel_confirm_technical_cancellation/generator.ts
+++ mock-service/src/config/mock-config/TRV14/2.0.0/cancel/cancel_confirm_technical_cancellation/generator.ts
@@ -1,15 +1,5 @@
-export async function cancelConfirmTechnicalCancellationGenerator(
-  existingPayload: any,
-  sessionData: any
-) {
-  if (sessionData.order_id) {
+export async function cancelConfirmTechnicalCancellationGenerator(existingPayload: any, sessionData: any) {
+  if(sessionData.order_id){
     existingPayload.message.order_id = sessionData.order_id;
   }
-
-  if (sessionData.flow_id === "technical_cancellation") {
-    existingPayload.message = {
-      ref_id: existingPayload?.context?.transaction_id ?? crypto.randomUUID(),
-    };
-  }
-  return existingPayload;
-}
+  return existingPayload;} 
